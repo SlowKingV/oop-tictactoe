4e7b2dec0199ef3c335bfc9b61c55043fbda778c
Diego Luna Granados <slowkingvii@gmail.com>
2020-02-25 11:07:13 -0600

    Add user interface and mocked logic

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,52 @@
 #!/usr/bin/env ruby
-puts 'Hello World!'
+puts 'TicTacToe Game...'
+
+print 'Player X - Enter you name: '
+name1 = gets.strip
+
+print 'Player O - Enter your name: '
+name2 = gets.strip
+
+board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
+
+puts "
+    1 2 3
+1 [ #{board[0][0]} #{board[0][1]} #{board[0][2]} ]
+2 [ #{board[1][0]} #{board[1][1]} #{board[1][2]} ]
+3 [ #{board[2][0]} #{board[2][1]} #{board[2][2]} ]
+----------------------------
+"
+
+print "#{name1} - Choose a row: "
+row = gets.strip.to_i
+
+print "#{name1} - Choose a column: "
+column = gets.strip.to_i
+
+board[row - 1][column - 1] = 'X'
+
+puts "
+    1 2 3
+1 [ #{board[0][0]} #{board[0][1]} #{board[0][2]} ]
+2 [ #{board[1][0]} #{board[1][1]} #{board[1][2]} ]
+3 [ #{board[2][0]} #{board[2][1]} #{board[2][2]} ]
+----------------------------
+"
+
+print "#{name2} - Choose a row: "
+row = gets.strip.to_i
+
+print "#{name2} - Choose a column: "
+column = gets.strip.to_i
+
+board[row - 1][column - 1] = 'O'
+
+puts "
+    1 2 3
+1 [ #{board[0][0]} #{board[0][1]} #{board[0][2]} ]
+2 [ #{board[1][0]} #{board[1][1]} #{board[1][2]} ]
+3 [ #{board[2][0]} #{board[2][1]} #{board[2][2]} ]
+----------------------------
+"
+
+puts 'Once you align three symbols you WIN!'
