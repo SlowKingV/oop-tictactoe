45f9ae926476f00f0d8be47b28f84589eb28d284
Diego Luna Granados <slowkingvii@gmail.com>
2020-02-27 14:13:40 -0600

    Optimze regexp and board

--- bin/main.rb
+++ bin/main.rb
@@ -1,12 +1,10 @@
 #!/usr/bin/env ruby
 puts 'TicTacToe Game...'
 WIN_CASES = [
-  'AAA[B\-]{6}',
-  '[B\-]{3}AAA[B\-]{3}',
-  '[B\-]{6}AAA',
-  '[B\-]{0,2}A[B\-]{2}A[B\-]{2}A[B\-]{0,2}',
-  '[B\-][B\-]A[B\-]A[B\-]A[B\-][B\-]',
-  'A[B\-][B\-][B\-]A[B\-][B\-][B\-]A'
+  '^(?:O{6}|O{3})?XXX(?:O{6}|O{3})?$',
+  '^[B\-]{0,2}(?:A[B\-]{2}){2}A[B\-]{0,2}$',
+  '^[B\-]{2}A[B\-]{3}[B\-]$',
+  '^(?:A[B\-]{3}){2}A$'
 ].freeze
 syms = %w[X O]
 name = ['Player X', 'Player O']
@@ -17,11 +15,11 @@ name[0] = gets.strip
 print 'Player O - Enter your name: '
 name[1] = gets.strip
 
-board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
+board = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
 winner = false
 
 def winner_move(sym1, sym2, brd)
-  win = WIN_CASES.any? { |v| Regexp.new(v.gsub(/[AB]/, 'A' => sym1, 'B' => sym2)).match?(brd.flatten.join) }
+  win = WIN_CASES.any? { |v| Regexp.new(v.gsub(/[AB]/, 'A' => sym1, 'B' => sym2)).match?(brd.join) }
   win
 end
 
@@ -33,26 +31,27 @@ while i < 9
 ----------------------------
 
     1 2 3
-1 [ #{board[0][0]} #{board[0][1]} #{board[0][2]} ]
-2 [ #{board[1][0]} #{board[1][1]} #{board[1][2]} ]
-3 [ #{board[2][0]} #{board[2][1]} #{board[2][2]} ]
+1 [ #{board[0]} #{board[1]} #{board[2]} ]
+2 [ #{board[3]} #{board[4]} #{board[5]} ]
+3 [ #{board[6]} #{board[7]} #{board[8]} ]
 
 "
   puts "#{name[current]} - Now it's your turn"
 
   print "#{name[current]} - Choose a column: "
-  col = gets.strip.to_i
+  col = gets.strip.to_i - 1
   print "#{name[current]} - Choose a row: "
-  row = gets.strip.to_i
+  row = gets.strip.to_i - 1
 
-  valid = ((1..3).include?(col) && (1..3).include?(row)) && (board[row - 1][col - 1] == '-')
+  pos = (3 * row) + col
+  valid = (0..8).include?(pos) && (board[pos] == '-')
 
   unless valid
     puts "\nWARNING - Your movement isn't valid:\nPlease, choose again...\n\n"
     next
   end
 
-  board[row - 1][col - 1] = syms[current]
+  board[pos] = syms[current]
   winner = winner_move(syms[current], syms[current - 1], board)
   break if winner
 
@@ -64,9 +63,9 @@ puts "
 ----------------------------
 
     1 2 3
-1 [ #{board[0][0]} #{board[0][1]} #{board[0][2]} ]
-2 [ #{board[1][0]} #{board[1][1]} #{board[1][2]} ]
-3 [ #{board[2][0]} #{board[2][1]} #{board[2][2]} ]
+1 [ #{board[0]} #{board[1]} #{board[2]} ]
+2 [ #{board[3]} #{board[4]} #{board[5]} ]
+3 [ #{board[6]} #{board[7]} #{board[8]} ]
 
 "
 puts winner ? "#{name[current]} wins! Congratulations!!!" : 'This is a Tie!'
